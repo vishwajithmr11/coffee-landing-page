961af4c033b06c46b4cef8d5b87dae0982a519ed
vishwajithmr11 <vishwajithmr9@gmail.com>
2026-04-03 18:35:21 +0530

    Created App.tsx structure for coffee landing page

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './components/Hero';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Hero />
+      {/* Other sections will go here */}
+    </div>
+  );
+}
+
+export default App;
