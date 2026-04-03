93f0ab8029577d789c8497ce50fbb598dbf2d876
vishwajithmr11 <vishwajithmr9@gmail.com>
2026-04-03 18:36:31 +0530

    Added FeaturedProducts to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from './components/Hero';
+import FeaturedProducts from './components/FeaturedProducts';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <FeaturedProducts />
       {/* Other sections will go here */}
     </div>
   );
